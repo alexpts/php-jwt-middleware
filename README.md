590e22722d61c8fb61e640c91a79b56107c5e54b
alexpts <alexpts@yandex.ru>
2018-05-18 21:22:33 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # php-jwt-middleware
+
+
+[![Build Status](https://travis-ci.org/alexpts/php-jwt-middleware.svg?branch=master)](https://travis-ci.org/alexpts/php-jwt-middleware)
+[![Code Coverage](https://scrutinizer-ci.com/g/alexpts/php-jwt-middleware/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/alexpts/php-jwt-middleware/?branch=master)
+[![Code Climate](https://codeclimate.com/github/alexpts/php-jwt-middleware/badges/gpa.svg)](https://codeclimate.com/github/alexpts/php-jwt-middleware)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/alexpts/php-jwt-middleware/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/alexpts/php-jwt-middleware/?branch=master)
+
+
+#### Install
+```composer require alexpts/php-jwt-middleware```
---
+++ src/PTS/CheckJwtToken/CheckJwtToken.php
@@ -0,0 +1,131 @@
+<?php
+declare(strict_types=1);
+
+namespace PTS\CheckJwtToken;
+
+use Emarref\Jwt\Token;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use PTS\JwtService\JwtService;
+
+class CheckJwtToken implements MiddlewareInterface
+{
+
+    /** @var string */
+    protected $tokenAttr = 'token';
+    /** @var string */
+    protected $ipAttr = 'client-ip';
+    /** @var string */
+    protected $cookieName = 'auth_token';
+    /** @var JwtService */
+    protected $jwtService;
+    /** @var bool */
+    protected $checkIp = true;
+
+    /**
+     * @param JwtService $jwtService
+     */
+    public function __construct(JwtService $jwtService)
+    {
+        $this->jwtService = $jwtService;
+    }
+
+    public function checkIp(bool $checkIp): self
+    {
+        $this->checkIp = $checkIp;
+        return $this;
+    }
+
+    public function setCookieName(string $cookieName): self
+    {
+        $this->cookieName = $cookieName;
+        return $this;
+    }
+
+    public function setIpAttr(string $attr): self
+    {
+        $this->ipAttr = $attr;
+        return $this;
+    }
+
+    public function setTokenAttr(string $attr): self
+    {
+        $this->tokenAttr = $attr;
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     * @throws TokenException
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
+    {
+        $jwtToken = $this->getTokenFromRequest($request);
+
+        if (null === $jwtToken) {
+            return $next->handle($request);
+        }
+
+        try {
+            $token = $this->jwtService->decode($jwtToken);
+            $this->jwtService->verify($token);
+
+            $clientIp = $request->getAttribute($this->ipAttr);
+            $this->checkTokenIp($token, $clientIp);
+
+            $request = $request->withAttribute($this->tokenAttr, $token);
+        } catch (\Exception $e) {
+            throw new TokenException('Bad token', 401, $e);
+        }
+
+        return $next->handle($request);
+    }
+
+    /**
+     * @param Token $token
+     * @param string|null $clientIp
+     *
+     * @throws TokenException
+     */
+    protected function checkTokenIp(Token $token, string $clientIp = null): void
+    {
+        if ($this->checkIp) {
+            $tokenIp = $token->getPayload()->findClaimByName('ip');
+            $tokenIp = $tokenIp !== null ? $tokenIp->getValue() : null;
+
+            if ($tokenIp && $clientIp !== $tokenIp) {
+                throw new TokenException('Token not valid for this ip');
+            }
+        }
+    }
+
+    protected function getTokenFromRequest(ServerRequestInterface $request): ?string
+    {
+        return $request->hasHeader('Authorization')
+            ? $this->getTokenFromBearerHeader($request)
+            : $this->getTokenFromCookie($request);
+    }
+
+    protected function getTokenFromBearerHeader(ServerRequestInterface $request): ?string
+    {
+        $header = $request->getHeader('Authorization');
+        [$type, $value] = explode(' ', $header[0]);
+
+        return $type === 'Bearer' ? $value : null;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return null|string
+     */
+    protected function getTokenFromCookie(ServerRequestInterface $request): ?string
+    {
+        $clientIp = $request->getAttribute($this->ipAttr);
+        $cookieName = $clientIp ? $this->cookieName . '_' . $clientIp : $this->cookieName;
+
+        return $request->getCookieParams()[$cookieName] ?? null;
+    }
+}
