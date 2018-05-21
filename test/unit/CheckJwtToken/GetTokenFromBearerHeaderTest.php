48c1a2505d04eba488ec9e2d3d3e40bef8e25633
alexpts <alexpts@yandex.ru>
2018-05-21 22:00:38 +0300

    unit test

---
+++ test/unit/CheckJwtToken/GetTokenFromBearerHeaderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use PTS\CheckJwtToken\CheckJwtToken;
+
+class GetTokenFromBearerHeaderTest extends TestCase
+{
+    /**
+     * @param null|string $expected
+     * @param array $header
+     *
+     * @throws ReflectionException
+     *
+     * @dataProvider dataProvider
+     */
+    public function testMethod(?string $expected, array $header): void
+    {
+        $request = $this->getMockBuilder(ServerRequestInterface::class)
+            ->setMethods(['getHeader'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $request->expects(self::once())->method('getHeader')->with('Authorization')->willReturn($header);
+
+        $middleware = $this->createMock(CheckJwtToken::class);
+
+        $method = new ReflectionMethod(CheckJwtToken::class, 'getTokenFromBearerHeader');
+        $method->setAccessible(true);
+        $actual = $method->invoke($middleware, $request);
+
+        self::assertSame($expected, $actual);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [null, [' ']],
+            ['aaa', ['Bearer aaa']],
+            [null, ['NotBearer bbb']],
+        ];
+    }
+}
